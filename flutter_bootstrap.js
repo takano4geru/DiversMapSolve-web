c1a1aaec6a14f8ee370142b43abff00eec0f1c3b
takano4geru <takano4geru@users.noreply.github.com>
2026-06-18 03:08:48 +0000

    deploy: takano4geru/DiversMapSolve@171d0d31b3aafd411f1303dcc015cdf750fec53e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1065264096" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3900968823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
